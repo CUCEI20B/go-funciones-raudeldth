e6767f0ca5f413a5abba628899e7241783d54edf
raudeldth <raudel.pereyra@alumnos.udg.mx>
2020-10-16 17:34:48 -0500

    implementando ordenamiento burbuja

--- burbuja.go
+++ burbuja.go
@@ -1,9 +1,20 @@
 package main
 
-func Burbuja(s []int64)  {
+import "fmt"
 
+func Burbuja(s []int64)  {
+	for i := 1; i < len(s); i++ {
+		for j := 0; j < len(s) - 1; j++ {
+			if s[j] > s[j+1] {
+				temp := s[j]
+				s[j] = s[j+1]
+				s[j+1] = temp
+			}
+		}
+	}
 }
 
 func main()  {
-	
+	a := []int64{8,4, 6, 3, 7, 10, 1, 2, 5, 9}
+	Burbuja(a)
 }
